05225e742c96e4af387332dfdd1df07f59b12db4
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-01-04 16:56:13 +0100

    Initial commit

---
+++ src/main/java/se/kry/hackdays/modulith/Application.java
@@ -0,0 +1,13 @@
+package se.kry.hackdays.modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformHackdaysModulithApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlatformHackdaysModulithApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/kry/hackdays/modulith/ApplicationTest.java
@@ -0,0 +1,13 @@
+package se.kry.hackdays.modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformHackdaysModulithApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
