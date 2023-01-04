80e343f1a3ecb4289f3550332c1127609b1d5f17
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-01-04 17:27:05 +0100

    Adding modulith

---
+++ src/main/java/se/kry/hackdays/modulith/payments/PaymentsModuleConfiguration.java
@@ -0,0 +1,7 @@
+package se.kry.hackdays.modulith.payments;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class PaymentsModuleConfiguration {
+}
---
+++ src/main/java/se/kry/hackdays/modulith/people/PeopleModuleConfiguration.java
@@ -0,0 +1,7 @@
+package se.kry.hackdays.modulith.people;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class PeopleModuleConfiguration {
+}
